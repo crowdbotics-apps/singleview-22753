e10a6b386f20513603e1422c658201e7140be69a
Crowdbotics <team@crowdbotics.com>
2020-12-01 04:41:44 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -10,6 +10,26 @@ class CustomText(models.Model):
     title = models.CharField(
         max_length=150,
     )
+    provider_name = models.TextField(
+        null=True,
+        blank=True,
+    )
+    contact = models.EmailField(
+        max_length=254,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
 
     def __str__(self):
         return self.title
@@ -24,7 +44,29 @@ class CustomText(models.Model):
 
 
 class HomePage(models.Model):
-    body = models.TextField()
+    quote_requests = models.TextField(
+        blank=True,
+    )
+    company = models.OneToOneField(
+        "home.CustomText",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="homepage_company",
+    )
+    email = models.EmailField(
+        max_length=254,
+        null=True,
+        blank=True,
+    )
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
 
     @property
     def api(self):
